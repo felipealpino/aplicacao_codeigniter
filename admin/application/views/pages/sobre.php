4d6726ad0124e74c454a08382245302135e9187f
Felipe Alpino <felipealpino1@gmail.com>
2021-02-24 23:26:16 -0300

    criando rotas para projeto B7web

---
+++ admin/application/views/pages/sobre.php
@@ -0,0 +1,2 @@
+<h2>página de sobre o meu site</h2> <br>
+ESSA É A PAGINA """"SOBRE"""""
---
+++ admin/application/views/templates/footer.php
@@ -0,0 +1,19 @@
+
+
+
+
+
+<footer style="background-color: rgb(90, 206, 167);
+    padding: 70px;
+    position: absolute;
+    width: 100%; 
+    bottom: 0;">
+        FOOTER
+</footer>
+
+
+    
+    
+    
+</body>
+</html>
